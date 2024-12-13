aa8d58c62c5e6a4ad344662780932a3e707872f5
Kacper Wiśniewski <wisniewski.kacper99@gmail.com>
2024-12-13 17:36:10 +0100

    Second commit in another branch

--- some-fun.utils.js
+++ some-fun.utils.js
@@ -1,18 +1,17 @@
 import moment from "moment";
 
 const calculateDiffToSaturday = (date) => {
-    if(typeof date !== "string")
-        throw new Error('Date format must be "YYYY-MM-DD"')
+  if (typeof date !== "string")
+    throw new Error('Date format must be "YYYY-MM-DD"');
 
-    return (6 - moment(date).day() + 7) % 7 || 7;
-}
+  return (6 - moment(date).day() + 7) % 7 || 7;
+};
 
 const calculateDiffToFriday = (date) => {
-    if(typeof date !== "string")
-        throw new Error('Date format must be "YYYY-MM-DD"')
+  if (typeof date !== "string")
+    throw new Error('Date format must be "YYYY-MM-DD"');
 
-    return (5 - moment(date).day() + 7) % 7 || 7;
-}
+  return (5 - moment(date).day() + 7) % 7 || 7;
+};
 
-
-export {calculateDiffToSaturday}
+export { calculateDiffToSaturday };
